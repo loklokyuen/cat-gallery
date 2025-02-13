a18f55cb15704e58b7cfdaa8db03ec31ef7a819c
loklokyuen <loklokyuen.m@gmail.com>
2025-02-13 23:30:31 +0000

    feat: add user signup form and save user info in local storage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,26 @@
-import { FormControl } from "@mui/material";
+import { Button } from "@mui/material";
+import Form from "./Form";
 
-export default function Home(){
+export default function Home({ user, setUser, setAvatarURL }){
     return <>
-        <FormControl>
-            Home
-        </FormControl>
+        <section>
+            <h2>Welcome to Cat Gallery!</h2>
+            <p>Discover adorable cat images from around the world.</p>
+            { user ? <>
+            <p>Great to see you again {user}!</p>
+            <Button 
+                variant="outlined"
+                color="primary"
+                sx={{margin: '10px', backgroundColor: '#fff', ':hover': {backgroundColor: '#7886C7', color:'#2D336B'}}}
+                onClick={(e)=>{ 
+                    localStorage.clear();
+                    setUser('');
+                    setAvatarURL('')
+                    }}>Clear my session</Button>
+            </>
+            :
+            <Form setUser={setUser} setAvatarURL={setAvatarURL}></Form>}
+
+        </section>
     </>
 }
